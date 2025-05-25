f2a21d6a8d9440b304e34be8bd734affb053ac0b
Maxime MAUSSION <maximemaussion30@gmail.com>
2025-05-25 20:36:27 +0200

    added cors , so i can fetch

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import jokeRoutes from './routes/jokeRoutes';
 import { errorHandler } from './middlewares/errorHandler';
+import cors from 'cors';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 // Routes
--- backend/src/models/joke.ts
+++ backend/src/models/joke.ts
@@ -4,4 +4,56 @@ export interface Joke {
   answer: string;
 }
 
-export const jokes: Joke[] = [];
+export const jokes: Joke[] = [
+  {
+    id: 1748000000001,
+    title: 'Quelle est la femelle du hamster ?',
+    answer: 'L’Amsterdam',
+  },
+  {
+    id: 1748000000002,
+    title: 'Que dit un oignon quand il se cogne ?',
+    answer: 'Aïe',
+  },
+  {
+    id: 1748000000003,
+    title: "Quel est l'animal le plus heureux ?",
+    answer: 'Le hibou, parce que sa femme est chouette.',
+  },
+  {
+    id: 1748000000004,
+    title: "Pourquoi le football c'est rigolo ?",
+    answer: 'Parce que Thierry en rit',
+  },
+  {
+    id: 1748000000005,
+    title: 'Quel est le sport le plus fruité ?',
+    answer:
+      'La boxe, parce que tu te prends des pêches dans la poire et tu tombes dans les pommes.',
+  },
+  {
+    id: 1748000000006,
+    title: 'Que se fait un Schtroumpf quand il tombe ?',
+    answer: 'Un Bleu',
+  },
+  {
+    id: 1748000000007,
+    title: 'Quel est le comble pour un marin ?',
+    answer: 'Avoir le nez qui coule',
+  },
+  {
+    id: 1748000000008,
+    title: "Qu'est ce que les enfants usent le plus à l'école ?",
+    answer: 'Le professeur',
+  },
+  {
+    id: 1748000000009,
+    title: 'Quel est le sport le plus silencieux ?',
+    answer: 'Le para-chuuuut',
+  },
+  {
+    id: 1748000000010,
+    title: 'Quel est le comble pour un joueur de bowling ?',
+    answer: 'C’est de perdre la boule',
+  },
+];
